6b9f90b8190d6c2ed69a4a677833c97cc0ebb90b
mi7ai <hottmayer@gmail.com>
2019-11-19 16:11:35 +0100

    Entregable 3: metodo factible en proceso

--- Ent/E3/entregable3.py
+++ Ent/E3/entregable3.py
@@ -1,6 +1,6 @@
 import sys
-from typing import Iterable
 from typing import *
+import random
 
 from Utils.bt_scheme import PartialSolution, BacktrackingSolver, Solution
 
@@ -32,6 +32,8 @@ def cryptosolve(lpalabras):
 			self.linea = linea
 			self.solution = solution
 			self.n = len(self.solution)
+			self.d = {}
+			self.usados = set()
 
 		def is_solution(self) -> bool:
 			pass
@@ -40,6 +42,10 @@ def cryptosolve(lpalabras):
 			pass
 
 		def successors(self) -> Iterable["PartialSolution"]:
+			# max_l = max(len(p1), len(p2))
+			# print(max_l)
+
+			# print(p1[5])
 			pass
 
 	for linea_palabras in lpalabras:
@@ -47,9 +53,53 @@ def cryptosolve(lpalabras):
 		return BacktrackingSolver.solve(initialps)
 
 
+def factible(lpalabras: List[str], d: dict):
+	s_local = 0
+	resto = 0
+	lpalabras = lpalabras
+	# for f in range(len(lpalabras[len(lpalabras) - 1])-1, -1, -1):  # la longitud de las columnas = la ultima palabra decr
+	for f in range(len(lpalabras[len(lpalabras) - 1])):
+		for c in range(len(lpalabras)):
+
+			letras = [letra for letra in reversed(lpalabras[c].strip())]  # cada letra de la palabra con indice c
+			print(d.get(letras[f]))
+			if d.get(letras[f]) is None:  # no hay valor asignado
+				return True
+			elif resto > 0:
+				s_local += resto
+
+			s_local += d.get(letras[f])
+
+			if c == len(lpalabras) - 1:  # si estoy en la ultma fila compruebo la suma
+				# comprobar si hay resto
+				if (s_local % 10) != s_local: #hay resto
+					resto += s_local % 10
+				# s_global = s_local
+				if s_local != letras[f]:
+					return False
+
+
+	# comprobar suma xq estan todas las palabras asignadas con un valor
+	return False
+
+
+def test():
+	# print(random.randint(1, 9))
+	d = {"x": 1}
+	print(d.get("a"))
+	print((9 + 2) % 10)  # si el resto es diferente que la suma , hay resto
+
+
 if __name__ == "__main__":
 	lista_palabras = datos_fichero(filename)
-	for sol in cryptosolve(lista_palabras):
-		print(sol)
+	# for sol in cryptosolve(lista_palabras):
+	# 	print(sol)
+	# test()
+	d = {}
+	d["a"] = 1
+	d["l"] = 2
+	d["b"] = 3
+	d["e"] = 4
+	d["n"] = 5
+	print(factible(lista_palabras[0], d))
 	print("\n<TERMINADO>")
-
