2f334168cab4f547b067cdbc83461208de48e053
mi7ai <hottmayer@gmail.com>
2020-05-05 11:55:23 +0200

    Repaso: ej completados p5

--- Repaso Examenes/enero2019/enero2019backtracking.py
+++ Repaso Examenes/enero2019/enero2019backtracking.py
@@ -2,7 +2,7 @@ from Utils.bt_scheme import BacktrackingOptSolver, BacktrackingSolver
 
 
 def numeros_solver(P, T):
-	class NumerosPS(BacktrackingSolver):
+	class NumerosPS(BacktrackingOptSolver):
 		def __init__(self, ds, suma_local):
 			self.ds = ds
 			self.n = len(ds)
@@ -14,37 +14,39 @@ def numeros_solver(P, T):
 		def get_solution(self):
 			return self.ds
 
-		# def state(self):
-		# 	return self.n, self.suma_local
+		def state(self):
+			return self.n, self.suma_local
 
-		# def f(self):
-		# 	sum = 0
-		# 	for i in range(len(self.ds)):
-		# 		if self.ds[i] == 1:
-		# 			sum += self.ds[i] * P[i]
-		# 		elif self.ds[i] == -1:
-		# 			sum += self.ds[i] * P[i]
-		# 	return sum
+		# lo que dice la f
+		def f(self):
+			sum = 0
+			for nr in self.ds:
+				if nr != 0:
+					sum += 1
+			return sum
 
 		def successors(self):
 			if self.n < len(P):
-				for i in range(self.n, len(P)):
-					nr = P[self.n]
-					if self.suma_local == T:
-						yield NumerosPS(self.ds + (0, ), self.suma_local)
-					elif self.suma_local + nr < T:
-						yield NumerosPS(self.ds + (-1,), self.suma_local - nr)
-					elif self.suma_local + nr > T:
-						yield NumerosPS(self.ds + (1,), self.suma_local + nr)
+				# for nr in P:
+				nr = P[self.n]
+
+				yield NumerosPS(self.ds + (0,), self.suma_local)
+				yield NumerosPS(self.ds + (1,), self.suma_local + nr)
+				yield NumerosPS(self.ds + (-1,), self.suma_local - nr)
 
 	initial_ps = NumerosPS((), 0)
-	return BacktrackingSolver.solve(initial_ps)
+	return BacktrackingOptSolver.solve(initial_ps)
 
 
 if __name__ == '__main__':
 	P = [12, 1, 10, 4, 9]
 	T = 6
 
+	haysol = False
 	for sol in numeros_solver(P, T):
+		haysol = True
 		print(sol)
+
+	if not haysol:
+		print("No hay solucion")
 
