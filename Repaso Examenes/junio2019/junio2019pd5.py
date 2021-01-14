142884dd3a283e554af9bf4ac0796f281bd46b8a
mi7ai <hottmayer@gmail.com>
2021-01-14 21:15:46 +0100

    added

---
+++ Repaso Examenes/junio2019/junio2019pd5.py
@@ -0,0 +1,32 @@
+from Utils.bt_scheme import infinity
+
+
+def g(f, c):
+	return f * c
+
+
+def fondos_rec_mem_camino(D, F, M):
+	def B(f, d):
+		if f == 0:
+			return 0
+
+		if (f, d) not in mem:
+			if f > 0 and d < M:
+				mem[f, d] = (B(f - 1, d), 0)
+			else:
+				mem[f, d] = max((B(f - 1, d - k) + g(f - 1, k), k) for k in range(M, d))
+			return mem[f, d][0]
+
+	mem = {}
+	sol = []
+	f, d = F, D
+	score = B(F, D)
+
+	return score, sol
+
+
+if __name__ == '__main__':
+	F = 4
+	D = 13
+	M = 2
+	print(fondos_rec_mem_camino(D, F, M))
