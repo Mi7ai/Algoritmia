5525d987f85cfd0f6628036bee0a93c411a71a12
mi7ai <hottmayer@gmail.com>
2021-01-14 21:15:07 +0100

    update

--- Testing/test.py
+++ Testing/test.py
@@ -1,2 +1,12 @@
+L = [1, 2, 5, 10]
+B = [100, 200, 500, 1000]
+P = [4, 5, 4, 2]
+M = 23
+n = len(L)
 
-print(8//5)
+for d in range(min(M // L[n - 1], P[n - 1]) + 1):
+	print(min(M // L[n - 1], P[n - 1]) + 1)
+	print(d)
+
+	M = (M - d * L[n - 1]) + d * B[n - 1]
+	n -= 1
