610e86a095f339fea63ab3c74b0f7ab1b383d96a
mi7ai <hottmayer@gmail.com>
2021-01-19 15:54:27 +0100

    updated not working

--- Repaso Examenes/enero2019/enero2019pd.py
+++ Repaso Examenes/enero2019/enero2019pd.py
@@ -5,12 +5,18 @@ def numeros_solver(P, T):
 	def L(n, t) -> int:
 		if n == 0:
 			return 0
-		# if n == 0 and t > 0:
-		# 	return infinity
-		if (n, t) not in mem:
-			if n > 0:
-				mem[n, t] = min((L(n - 1, t - d * P[n - 1]) + d, d) for d in range(-1, 2))
+		if n == 0 and t > 0:
+			return infinity
 
+		if (n, t) not in mem:
+
+			mem[n, t] = min((L(n - 1, t - abs(d) * P[n - 1]) + d, d) for d in range(-1, 2))
+
+			# elif n > 0 and t <= P[n-1]:
+			# 	mem[n, t] = (L(n - 1, t - 1 * P[n - 1] - 1), -1)
+
+			# else:
+			# 	mem[n, t] = (L(n-1,t),0)
 		return mem[n, t][0]
 
 	mem = {}
--- Testing/test.py
+++ Testing/test.py
@@ -3,10 +3,13 @@ B = [100, 200, 500, 1000]
 P = [4, 5, 4, 2]
 M = 23
 n = len(L)
+#
+# for d in range(min(M // L[n - 1], P[n - 1]) + 1):
+# 	print(min(M // L[n - 1], P[n - 1]) + 1)
+# 	print(d)
+#
+# 	M = (M - d * L[n - 1]) + d * B[n - 1]
+# 	n -= 1
 
-for d in range(min(M // L[n - 1], P[n - 1]) + 1):
-	print(min(M // L[n - 1], P[n - 1]) + 1)
-	print(d)
-
-	M = (M - d * L[n - 1]) + d * B[n - 1]
-	n -= 1
+for i in range(-1,2):
+	print(i)
