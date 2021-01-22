1e5a170c0cda2089bfd752e194f3fb3bd4267a1e
mi7ai <hottmayer@gmail.com>
2021-01-22 14:37:41 +0100

    updated

--- Repaso Examenes/enero2015/enero2015voraces.py
+++ Repaso Examenes/enero2015/enero2015voraces.py
@@ -7,7 +7,21 @@ def galeria(X, D):
 	return res
 
 
+def galeria2(X, D):
+	res = []
+	if len(X) > 1:
+		res.append(X[0])
+	else:
+		return None
+
+	for i in range(len(X)):
+		if res[-1] + D < X[i]:
+			res.append(X[i])
+	return res
+
+
 if __name__ == '__main__':
-	X = [2, 3, 4, 7, 8, 9, 13]
+	X = [2, 3, 4, 5, 7, 8, 9, 13, 15]
 	D = 2
-	print(galeria(X, D))
+	print(galeria(X, D),"este no va bien")
+	print(galeria2(X, D))
