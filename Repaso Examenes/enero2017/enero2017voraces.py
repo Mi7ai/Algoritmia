7d427b9c78c426ea6560b45a5e35d48afb4f3833
mi7ai <hottmayer@gmail.com>
2021-01-22 19:07:14 +0100

    updated

--- Repaso Examenes/enero2017/enero2017voraces.py
+++ Repaso Examenes/enero2017/enero2017voraces.py
@@ -11,7 +11,6 @@ def numeros(V, a):
 		# nr actual
 		nr = V[v[i]]
 		if nr > fin:
-
 			ini = nr
 			fin = ini + a
 			res.append([ini, fin])
@@ -20,7 +19,24 @@ def numeros(V, a):
 	return res
 
 
+def numeros2(f, a):
+	res = []
+	ini = f[0]
+	fin = ini + a
+	res.append([ini, fin])
+	indices_ordenados = sorted(range(len(V)), key=lambda i: f[i])
+
+	for i in indices_ordenados:
+		if f[i] > fin:
+			ini = f[i]
+			fin = ini + a
+			res.append([ini, fin])
+
+	return res
+
+
 if __name__ == '__main__':
 	V = [1.0, 3.5, 9.0, 2.5, 7.0, 12.0, 13.0, 15.0]
 	A = 3.0
 	print(numeros(V, A))
+	print(numeros2(V, A))
