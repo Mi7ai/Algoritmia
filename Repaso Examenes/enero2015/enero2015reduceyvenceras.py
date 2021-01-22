a71f9fbb7e62f11b7f458a5b8220c234642276ac
mi7ai <hottmayer@gmail.com>
2021-01-22 19:07:46 +0100

    updated not working

--- Repaso Examenes/enero2015/enero2015reduceyvenceras.py
+++ Repaso Examenes/enero2015/enero2015reduceyvenceras.py
@@ -1,18 +1,20 @@
 def busca_indice(v):
 	def solve(i, j):
 		if j - i <= 1:
-			return 1
+			return i
 
 		m = (i + j) // 2
 		if v[m] != v[m - 1] and v[m] != v[m + 1]:
 			return v[m]
-		elif v[m] != v[m-1]:
+		elif v[m] > v[m - 1]:
 			return solve(i, m)
-		else:
-			return solve(m+1, j)
+		elif v[m] <= v[m - 1]:
+			return solve(m + 1, j)
+
 	return solve(0, len(v))
 
 
 if __name__ == '__main__':
-	v = [1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9]
-	print(busca_indice(v))
+	# v = [1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9]
+	v = [1, 1, 2, 3, 3, 4, 4, 5, 5]
+	print(busca_indice(v), "pero tiene que decir 2")
