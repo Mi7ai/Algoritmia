369a610e037a8a339fc3205467685e4a355e265f
mi7ai <hottmayer@gmail.com>
2021-01-24 19:50:13 +0100

    updated

--- Repaso Examenes/junio2019/junio2019pd5.py
+++ Repaso Examenes/junio2019/junio2019pd5.py
@@ -33,8 +33,35 @@ def fondos_rec_mem_camino(D, F, M):
 	return score, sol
 
 
+def fondos_rec_mem_camino2(D, F, M):
+	def B(f, d):
+		if f == 0:
+			return 0
+
+		if (f, d) not in mem:
+			if f > 0 and d < M:
+				mem[f, d] = (B(f - 1, d), (f-1,d,0))
+			else:
+				mem[f, d] = max((B(f - 1, d - k) + g(f - 1, k), (f - 1, d - k, k)) for k in range(M, d + 1))
+
+		return mem[f, d][0]
+
+	mem = {}
+	sol = []
+	f, d = F, D
+	score = B(F, D)
+
+	while f > 0:
+		f, d, k = mem[f, d][1]
+		sol.append(k)
+
+	sol.reverse()
+	return score, sol
+
+
 if __name__ == '__main__':
 	F = 4
 	D = 13
 	M = 2
 	print(fondos_rec_mem_camino(D, F, M))
+	print(fondos_rec_mem_camino2(D, F, M))
